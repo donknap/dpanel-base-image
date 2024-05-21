45d3ee6140db7021f37e4ef6433dbd0c12d5e08a
donknap@gmail.com <donknap@gmail.com>
2024-05-21 18:03:54 +0800

    整理目录卷

--- php/wwwroot/index.php
+++ php/wwwroot/index.php
@@ -32,15 +32,15 @@
     <h1>恭喜, 站点创建成功！</h1>
     <h3>这是默认index.php,本页面由系统自动生成。</h3>
     <ul>
-      <li>当前 php 版本：<?php echo phpversion(); ?>，默认网站目录:/home/wwwroot, nginx 配置目录: /home/nginx </li>
+      <li>当前 php 版本：<?php echo phpversion(); ?>，默认网站目录:/app/wwwroot, nginx 配置目录: /app/nginx </li>
       <li>当前 php 扩展：
         <blockquote class="blockquote" style="word-wrap:break-word;  ">
           <?php echo implode(", ", get_loaded_extensions()); ?>
         </blockquote>
       </li>
       <li>缺少的扩展可以再次编译构建 Dockerfile 进行增加</li>
-      <li>您可以通过【容器管理】进入该容器， 交通过【文件管理】功能，上传代码至 /home/wwwroot 目录，完成功部署</li>
-      <li>也可以通过修改 /home/nginx 目录中的配置，增加需要的配置</li>
+      <li>您可以通过【容器管理】进入该容器， 交通过【文件管理】功能，上传代码至 /app/wwwroot 目录，完成功部署</li>
+      <li>也可以通过修改 /app/nginx 目录中的配置，增加需要的配置</li>
       <li>线上部署时请务必删除此文件</li>
     </ul>
   </div>
